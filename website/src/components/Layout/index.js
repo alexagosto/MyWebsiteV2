c25de56353fed687d2f0bef04a3fbe33546a9f84
Alex <alex.agosto@upr.edu>
2024-02-23 19:51:43 -0400

    Styling for Layout.

--- website/src/components/Layout/index.js
+++ website/src/components/Layout/index.js
@@ -1,8 +1,22 @@
 import './index.scss';
 import Navbar from '../Navbar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-  return <><Navbar /></>
+  return( <div className={"App"}>
+    <Navbar />
+    <div className={'page'}>
+      <span className={'tags top-tags'}>&lt;body&gt;</span>
+
+      <Outlet />
+
+      <span className={'tags bottom-tags'}>
+        &lt;/body&gt;
+        <br />
+        <span className={'bottom-tags-html'}>&lt;/html&gt;</span>
+      </span>
+    </div>
+  </div>)
 }
 
 export default Layout
