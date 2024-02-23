dc33a00b3df15e8110770b0963bebc74c5185469
Alex <alex.agosto@upr.edu>
2024-02-23 19:04:53 -0400

    Basic Structure of the website's assets, fonts. Also included Navbar and routes.
    Fonts arent changing for some reason, figure out later.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyWebsiteV2
 Revamped version of my website, ideally a complete overhaul; this time with react instead of vue.
+ 
---
+++ website/src/components/Navbar/index.js
@@ -0,0 +1,54 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoName from '../../assets/mylogos/FullNameBlackOnWhite.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faPaintBrush, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Navbar = () => (
+  <div className='nav-bar'>
+    <Link className="logo" to={'/'}>
+      <img className={'logoName'} src={LogoName} alt="fullname" />
+    </Link>
+
+    <nav>
+      <NavLink exact={"true"} activeClassName={'active'} to={"/"} data-text={"HOME"}>
+        <FontAwesomeIcon icon={faHome} color = "#FFFFFF" />
+      </NavLink>
+
+      <NavLink exact={"true"} activeClassName={'active'} className="about-link" to={"/about"} data-text={"ABOUT"}>
+        <FontAwesomeIcon icon={faUser} color = "#FFFFFF" />
+      </NavLink>
+
+      <NavLink exact={"true"} activeClassName={'active'} className="contact-link" to={"/contact"} data-text={"CONTACT"}>
+        <FontAwesomeIcon icon={faEnvelope} color = "#FFFFFF" />
+      </NavLink>
+
+      <NavLink exact={"true"} activeClassName={'active'} className="paintings-link" to={"/paintings"} data-text={"PAINTINGS"}>
+        <FontAwesomeIcon icon={faPaintBrush} color = "#FFFFFF" />
+      </NavLink>
+    </nav>
+
+    <ul>
+      <li>
+        <a target={"_blank"} rel={"noreferrer"} href={"https://www.linkedin.com/in/alex-agosto/"}>
+          <FontAwesomeIcon icon={faLinkedin} color="#FFFFFF" />
+        </a>
+      </li>
+
+      <li>
+        <a target={"_blank"} rel={"noreferrer"} href={"https://github.com/alexagosto"}>
+          <FontAwesomeIcon icon={faGithub} color="#FFFFFF" />
+        </a>
+      </li>
+
+      <li>
+        <a target={"_blank"} rel={"noreferrer"} href={"https://www.instagram.com/alex__augustus/"}>
+          <FontAwesomeIcon icon={faInstagram} color="#FFFFFF" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Navbar
