805a813c7a0ab3600a7de508872a5409b8b56175
Alex <alex.agosto@upr.edu>
2024-02-26 20:51:06 -0400

    Fixed home logo that was dissapeared for some reason.

--- website/src/components/Home/Logo/index.js
+++ website/src/components/Home/Logo/index.js
@@ -1,14 +1,9 @@
 import './index.scss'
 import LogoS from '../../../assets/mylogos/AcronymFillBlack.png'
-import {useRef } from 'react'
 const Logo = () => {
-
-  const bgRef = useRef();
-  const solidLogoRef = useRef();
-
   return(
-    <div className="logo-container" ref={bgRef}>
-      <img ref={solidLogoRef} className={'solid-logo'} src={LogoS} alt={'Acronym'} />
+    <div className="logo-container">
+      <img className={'solid-logo'} src={LogoS} alt={'Acronym'} />
     </div>
   )
 }
